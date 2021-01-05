c489927ffc306311c57b351dcc59ef9c5ebd7cd7
Maciej <maciej.beresewicz@gmail.com>
2021-01-05 23:20:49 +0100

    ffirms

--- codes/01-initial-codes.jl
+++ codes/01-initial-codes.jl
@@ -5,5 +5,5 @@ using DelimitedFiles
 m1 = vcat(readlines("./data-raw/kprm-new-data.json"), readlines("./data-raw/kprm-old-data.json"))
 firmy = [JSON3.read(i)[3] for i in m1]
 
-unique(firmy) |> sort |> (x -> writedlm("data/kprm-firmy.txr", x))
+unique(firmy) |> sort |> (x -> writedlm("data/kprm-firmy.txt", x))
 
